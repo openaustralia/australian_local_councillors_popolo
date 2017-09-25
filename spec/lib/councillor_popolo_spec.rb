792b5664f99569333b0dd9fe4a14d228e055b71e
Luke Bacon <luke@equivalentideas.com>
2017-09-25 22:07:36 +1000

    Restructure test code for readability

--- spec/lib/councillor_popolo_spec.rb
+++ spec/lib/councillor_popolo_spec.rb
@@ -5,11 +5,14 @@ describe CouncillorPopolo do
   # Check that there's no unexpected hanging changes
   it "all changes in data/**/*.csv files have been generated into Popolo JSON" do
     STATES.each do |state|
+      csv = CouncillorPopolo.csv_path_for_state(state)
       json_from_csv_file = JSON.pretty_generate(
-        Popolo::CSV.new(csv_path_for_state(state)).data
+        Popolo::CSV.new(csv).data
       )
 
-      expect(json_from_csv_file).to eql File.read(json_path_for_state(state))
+      expect(json_from_csv_file).to eql File.read(
+        CouncillorPopolo.json_path_for_state(state)
+      )
     end
   end
 end
