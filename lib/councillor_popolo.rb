2ca0af30a173be82498eb111bec70e38b65b4ad8
Luke Bacon <luke@equivalentideas.com>
2017-09-26 16:17:42 +1000

    Improve performance of method
    By pre-checking if there are non-unique ids to collect.

--- lib/councillor_popolo.rb
+++ lib/councillor_popolo.rb
@@ -30,10 +30,15 @@ class CouncillorDataProcessor
 
   def duplicate_councillor_ids_in_state_csv
     csv = CSV.read(csv_path_for_state, headers: :first_row)
+    ids = []
 
-    csv.values_at("id").flatten.select do |id|
-      id unless csv.values_at("id").flatten.one? {|id2| id.eql? id2}
-    end.uniq
+    if csv.values_at("id").count != csv.values_at("id").uniq.count
+      ids = csv.values_at("id").flatten.select do |id|
+        id unless csv.values_at("id").flatten.one? {|id2| id.eql? id2}
+      end.uniq
+    end
+
+    ids
   end
 
   def json_path_for_state
