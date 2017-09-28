6db75af82810e6344c1e2b04a99b8a64545b9c45
Luke Bacon <luke@equivalentideas.com>
2017-09-28 14:25:55 +1000

    Use more specific language in variable name

--- lib/councillor_csv_merger.rb
+++ lib/councillor_csv_merger.rb
@@ -1,9 +1,9 @@
 require('csv')
 
 class CouncillorCSVMerger
-  def self.merge(master_file_path, changes_file_path)
-    CSV.open(master_file_path, "a+", headers: true) do |master_csv|
-      CSV.read(changes_file_path, "r", headers: true).each do |changes_row|
+  def self.merge(master_csv_path, changes_csv_path)
+    CSV.open(master_csv_path, "a+", headers: true) do |master_csv|
+      CSV.read(changes_csv_path, "r", headers: true).each do |changes_row|
         master_csv << changes_row
       end
     end
