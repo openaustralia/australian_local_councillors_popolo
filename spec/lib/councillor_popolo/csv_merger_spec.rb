cfc9acd852f7d6cc5da964666ea18614f30da555
Luke Bacon <luke@equivalentideas.com>
2017-10-02 23:28:08 +1100

    Narrow scope of test setup
    Not needed for the initialize specs.

--- spec/lib/councillor_popolo/csv_merger_spec.rb
+++ spec/lib/councillor_popolo/csv_merger_spec.rb
@@ -1,47 +1,46 @@
 require('./lib/councillor_popolo')
 
 describe CouncillorPopolo::CSVMerger do
-  let(:master_csv_path) { "./spec/fixtures/local_councillors_master.csv" }
-  let(:changes_csv_path) { "./spec/fixtures/local_councillors_changes.csv" }
-  let(:csv_headers) { ["name", "start_date", "end_date", "executive", "council", "council_website", "id", "email", "image", "party", "source", "ward"] }
-  let(:pre_existing_councillor_row) { ["Julia Chessell", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/julia_chessell", "jches@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/julia.jpg", "Independent", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
-  let(:henare) { ["Henare Degan", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/henare_degan", "hdegan@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/henare.jpg", "Party Party Party", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
-  let(:hisayo) { ["Hisayo Horie", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/hisayo_horie", "hhorie@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/hisayo.jpg", "Make Toronto Nice Party", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
-  let(:new_councillor_rows) do
-    [ henare, hisayo ]
-  end
-
-  before do
-    CSV.open(master_csv_path, "w") do |csv|
-      csv << csv_headers
-      csv << pre_existing_councillor_row
-    end
-
-    CSV.open(changes_csv_path, "w") do |csv|
-      csv << csv_headers
-      new_councillor_rows.each do |row|
-        csv << row
-      end
-    end
-  end
-
-  after do
-    File.delete(master_csv_path)
-    File.delete(changes_csv_path)
-  end
-
   it "won't initialize without a master_csv_path" do
-    expect { CouncillorPopolo::CSVMerger.new(changes_csv_path: changes_csv_path) }.
+    expect { CouncillorPopolo::CSVMerger.new(changes_csv_path: "file.csv") }.
       to raise_error(ArgumentError, "missing keyword: master_csv_path")
   end
 
   it "won't initialize without a changes_csv_path" do
-    expect { CouncillorPopolo::CSVMerger.new(master_csv_path: master_csv_path) }.
+    expect { CouncillorPopolo::CSVMerger.new(master_csv_path: "file.csv") }.
       to raise_error(ArgumentError, "missing keyword: changes_csv_path")
   end
 
-
   describe ".merge" do
+    let(:master_csv_path) { "./spec/fixtures/local_councillors_master.csv" }
+    let(:changes_csv_path) { "./spec/fixtures/local_councillors_changes.csv" }
+    let(:csv_headers) { ["name", "start_date", "end_date", "executive", "council", "council_website", "id", "email", "image", "party", "source", "ward"] }
+    let(:pre_existing_councillor_row) { ["Julia Chessell", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/julia_chessell", "jches@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/julia.jpg", "Independent", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
+    let(:henare) { ["Henare Degan", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/henare_degan", "hdegan@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/henare.jpg", "Party Party Party", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
+    let(:hisayo) { ["Hisayo Horie", "", "", "", "Foo City Council", "http://www.foo.nsw.gov.au", "foo_city_council/hisayo_horie", "hhorie@foocity.nsw.gov.au", "http://www.foo.nsw.gov.au/__data/assets/image/0018/11547/hisayo.jpg", "Make Toronto Nice Party", "http://www.foo.nsw.gov.au/inside-foo/about-council/councillors", ""] }
+    let(:new_councillor_rows) do
+      [ henare, hisayo ]
+    end
+
+    before do
+      CSV.open(master_csv_path, "w") do |csv|
+        csv << csv_headers
+        csv << pre_existing_councillor_row
+      end
+
+      CSV.open(changes_csv_path, "w") do |csv|
+        csv << csv_headers
+        new_councillor_rows.each do |row|
+          csv << row
+        end
+      end
+    end
+
+    after do
+      File.delete(master_csv_path)
+      File.delete(changes_csv_path)
+    end
+
     it "validates the changes CSV" do
       merger = CouncillorPopolo::CSVMerger.new(
         master_csv_path: master_csv_path,
